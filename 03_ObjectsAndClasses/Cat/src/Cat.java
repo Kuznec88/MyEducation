2d99d52f1d39ffe730c6397f0a332edec89f75a7
Your Name <89165418685@mail.ru>
2020-07-04 04:42:00 +0300

    доделал счетчик + задание со звездочкой

--- 03_ObjectsAndClasses/Cat/src/Cat.java
+++ 03_ObjectsAndClasses/Cat/src/Cat.java
@@ -1,6 +1,6 @@
+import java.util.Objects;
 
-public class Cat
-{
+public class Cat {
     public static int catCount; // правильно понял что переменную для счетчика надо засунуть сюда?
     private double originWeight;
     private double weight;
@@ -9,60 +9,95 @@ public class Cat
     private double maxWeight;
     private double mealCount; // создаем переменную для счетчика еды
 
-    public Cat()
-    {
+    public boolean isAlive = (weight <= maxWeight && weight >= minWeight); // здесь создал булеан проверяющий жива кошка или нет
+
+    public Cat() {
         weight = 1500.0 + 3000.0 * Math.random();
         originWeight = weight;
         minWeight = 1000.0;
         maxWeight = 9000.0;
         catCount++; // а сюда засунуть сам счетчик?
     }
+
     public void pee() // создаем метод
     {
         weight = weight - 20; // при вызове метода вес кошака меняется на -20
         System.out.println("Agghhh"); // печатаем в консоль эмоции кошки
     }
-    public void meow()
-    {
-        weight = weight - 1;
-        System.out.println("Meow");
+
+    public void meow() {
+
+        if (isAlive = true);{
+
+            weight = weight - 1;
+
+            System.out.println("Meow");
+
+            if (weight < minWeight) {
+
+                catCount--;
+
+                if (isAlive = false) {
+
+                    System.out.println("Кошка мертва"); // здесь поидее должна выводиться надпись, но т.к у нас метод войд, она не выводится
+                }
+            }
+        }
     }
 
-    public void feed(Double amount)
-    {
+        public void feed (Double amount) {
+            if (isAlive = true) {
+
+                weight = weight + amount;
+
+                mealCount += amount; // счетчик еды
+
+                if (weight < maxWeight){
+
+                    catCount--;
+
+                    if (isAlive = false){
+
+                        System.out.println("Кошак мертв");
+                    }
+                }
+            }
+        }
+
+        public void drink (Double amount){
+
+        if (weight <= maxWeight && weight >= minWeight) {
+
         weight = weight + amount;
+
         mealCount += amount; // счетчик еды
-    }
 
-    public void drink(Double amount)
-    {
-        weight = weight + amount;
-    }
+        if (weight < maxWeight){
 
-    public double getWeight()
-    {
-        return weight;
+            catCount--;
+        }
     }
-    public double mealCount() // метод по возврату съеденной еды
-    {
-        return mealCount;
-    }
-    public String getStatus()
-    {
-        if(weight < minWeight) {
-            return "Dead";
-        }
-        else if(weight > maxWeight) {
-            return "Exploded";
-        }
-        else if(weight > originWeight) {
-            return "Sleeping";
-        }
-        else{
-            return "Playing";
-        }
-
-    }
-
 }
+        public double getWeight ()
+        {
+            return weight;
+        }
+        public double mealCount () // метод по возврату съеденной еды
+        {
+            return mealCount;
+        }
+        public String getStatus ()
+        {
+            if (weight < minWeight) {
+                return "Dead";
+            } else if (weight > maxWeight) {
+                return "Exploded";
+            } else if (weight > originWeight) {
+                return "Sleeping";
+            } else {
+                return "Playing";
+            }
+        }
+    }
+
 
--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -7,8 +7,6 @@ public class Loader {
         Cat manowar = new Cat(); // создали кота
         Cat rhcp = new Cat(); // создали кота
 
-
-
         Cat maffin = new Cat(); // создали кота
         System.out.println("Вес Маффина " + maffin.getWeight()); // получаем вес кота и выводим в консоль
         maffin.feed(9001.00 - maffin.getWeight()); // кормим ровно тем количеством корма + 1 чтобы кот взорвлся, без цикла
@@ -18,9 +16,9 @@ public class Loader {
 
         Cat ioko = new Cat(); // создали кота
         System.out.println("Вес Йоко " + ioko.getWeight()); // получаем вес и выводим в консоль
-        while (ioko.getStatus() != "Dead") { // кошка мяукает пока статус не будет деад, без break)
+        while (!ioko.getStatus().equals("Dead")) { // кошка мяукает пока статус не будет деад, без break)
             ioko.meow(); // мяукает
-            System.out.println("Йоко " + ioko.getStatus()); // проверяем статус, при смерти перестает мяукать
+            System.out.println("Йоко " + ioko.getStatus());// проверяем статус, при смерти перестает мяукать
 
 
             // 2 Задание
@@ -33,8 +31,6 @@ public class Loader {
             rhcp.pee();
             System.out.println("Кот рхчп за все время съел " + rhcp.mealCount() + " грамм корма");  // проверка кол-ва еды
             System.out.println("Всего котов создано " + Cat.catCount); // вызываем счетчик котов при помощи слова Cat
-            System.out.println();
-
         }
     }
 }
