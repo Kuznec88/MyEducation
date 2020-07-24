d6781a67f501466c84b56d1726dedabde283abcc
Your Name <89165418685@mail.ru>
2020-07-24 05:22:23 +0300

    HW 4.3 first part

--- 04_NumbersStringsAndDates/GeometryCalculator/src/GeometryCalculator.java
+++ 04_NumbersStringsAndDates/GeometryCalculator/src/GeometryCalculator.java
@@ -1,21 +1,45 @@
 public class GeometryCalculator {
     // метод должен использовать абсолютное значение radius
-    public static double getCircleSquare(double radius) {
-        return 0;
+    public static double getCircleSquare(double radius)
+    {
+        return 4 * Math.PI * (radius * radius); // расчет площади через радиус
     }
 
     // метод должен использовать абсолютное значение radius
-    public static double getSphereVolume(double radius) {
-        return 0;
+    public static double getSphereVolume(double radius)
+    {
+        return 4/3 * Math.PI * (radius * radius * radius); // расчет объема через радиус
     }
 
-    public static boolean isTriangleRightAngled(double a, double b, double c) {
-        return false;
+    public static boolean isTriangleRightAngled(double a, double b, double c) // в этом методе есть ощущение что накосячил, но вроде не косячил
+    {
+        if (a + b > c || b + c > a || c + a > b)
+        {
+            return true;
+        }
+        else if(a + b == c || a + c == b || b + c == a)
+        {
+            return false;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
     // методом isTriangleRightAngled, если невозможен вернуть -1.0
-    public static double getTriangleSquare(double a, double b, double c) {
-        return 0;
+    public static double getTriangleSquare(double a, double b, double c) // расчет площади треугольника по формуле герона
+    {
+        double HalfPerimetr = (a + b + c) / 2;
+
+        if (isTriangleRightAngled(a, b, c))
+        {
+            return Math.sqrt(HalfPerimetr * (HalfPerimetr - a) * (HalfPerimetr - b) * (HalfPerimetr - c));
+        }
+            else
+            {
+            return -1;
+            }
     }
 }
--- 04_NumbersStringsAndDates/GeometryCalculator/src/Main.java
+++ 04_NumbersStringsAndDates/GeometryCalculator/src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Circle Square");
-        System.out.println(GeometryCalculator.getCircleSquare(3d));
-        System.out.println(GeometryCalculator.getCircleSquare(-5));
-        System.out.println(GeometryCalculator.getCircleSquare(6.56));
+        System.out.println(GeometryCalculator.getCircleSquare(Math.abs(3d)));
+        System.out.println(GeometryCalculator.getCircleSquare(Math.abs(-5)));
+        System.out.println(GeometryCalculator.getCircleSquare(Math.abs(6.56)));
 
         System.out.println("\nSphere Volume");
-        System.out.println(GeometryCalculator.getSphereVolume(1d));
-        System.out.println(GeometryCalculator.getSphereVolume(57.6));
-        System.out.println(GeometryCalculator.getSphereVolume(0));
+        System.out.println(GeometryCalculator.getSphereVolume(Math.abs(1d)));
+        System.out.println(GeometryCalculator.getSphereVolume(Math.abs(57.6)));
+        System.out.println(GeometryCalculator.getSphereVolume(Math.abs(0)));
 
         System.out.println("\nis Triangle Right");
         System.out.println(GeometryCalculator.isTriangleRightAngled(0, 0, 0));
