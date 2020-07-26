0377fd0469a07d10315a28bd8a011c93c70bb5a9
Your Name <89165418685@mail.ru>
2020-07-26 14:10:32 +0300

    4.3 changes

--- 04_NumbersStringsAndDates/GeometryCalculator/src/GeometryCalculator.java
+++ 04_NumbersStringsAndDates/GeometryCalculator/src/GeometryCalculator.java
@@ -2,13 +2,13 @@ public class GeometryCalculator {
     // метод должен использовать абсолютное значение radius
     public static double getCircleSquare(double radius)
     {
-        return 4 * Math.PI * (Math.pow(radius, 2)); // расчет площади через радиус
+        return 4 * Math.PI * (Math.pow(Math.abs(radius), 2)); // расчет площади через радиус
     }
 
     // метод должен использовать абсолютное значение radius
     public static double getSphereVolume(double radius)
     {
-        return 4/3 * Math.PI * (Math.pow(radius, 3)); // расчет объема через радиус
+        return 4/3 * Math.PI * (Math.pow(Math.abs(radius), 3)); // расчет объема через радиус
     }
 
     public static boolean isTriangleRightAngled(double a, double b, double c) // в этом методе есть ощущение что накосячил, но вроде не косячил
---
+++ 04_NumbersStringsAndDates/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int truck;
+        int container;
+        int box;
+    }
+
+}
