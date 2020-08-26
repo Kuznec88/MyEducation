1f1eb6b1f011acfd2cb9e9e384090a08090508c5
Your Name <89165418685@mail.ru>
2020-08-27 01:29:01 +0300

    HomeWork 5.2

--- 05_ArraysAndCollections/src/src/ModulFive.java
+++ 05_ArraysAndCollections/src/src/ModulFive.java
@@ -1,49 +1,45 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ModulFive {
-
-    private static final int PACIENTS_COUNT = 30; // коснтанта кол-во пациентов
-
-    private static final double MIN_TEMPERATURE = 32.00; // константа минимальная температура
-    private static final double MAX_TEMPERATURE = 40.00; // константа максимальная температура
-
-    private static final double MIN_NORMALTEMPERATURE = 36.2; // константа минимальная температура для здорового человека
-    private static final double MAX_NORMALTEMPERATURE = 36.9; // коонстанта максимальная температура для здорового человека
-
-
     public static void main(String[] args) {
-        int countHealty = 0;
 
-        double[] pacients = new double[PACIENTS_COUNT]; // создаем массив с константой кол-во пациентов
-        for (int i = 0; i < pacients.length; i++) { // перебираем массив
-            final double rnd = rnd(MIN_TEMPERATURE, MAX_TEMPERATURE); // создаем переменную определяющую минимальную и максимальную температуру
-            pacients[i] = rnd; // присваиваем каждому пациенту температуру
-            System.out.println(pacients[i]); // выводим в консоль
+        ArrayList<String> todoList = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите команду: ");
+        String instruction = scanner.nextLine();
+        String information = scanner.nextLine();
+        String element = scanner.nextLine();
+
+// todoList.add("Привет");
+// todoList.add("Как дела");
+//todoList.remove(0);
+
+        if (instruction.matches("list"))
+        {
+            todoList.size();
+            System.out.println("Список дел: " + todoList.toString()); // сделать чтоб вводилось кореектно
+            scanner.next();
         }
 
-        for (int b = 0; b < pacients.length; b++) { // перебираем массив
-            if (pacients[b] > MIN_NORMALTEMPERATURE && pacients[b] < MAX_NORMALTEMPERATURE) // чтоб понять какой пациент здоров исходя из заданных температур
-            {
-                System.out.println("Пациент № " + b + " с температурой: " + pacients[b] + " - здоров"); // выводим
-                ++countHealty;
-            }
+        if (instruction.matches("add"))
+        {
+            todoList.add(information);
         }
-        double average = 0; // пеерменная для среднего арифметического
-        if (pacients.length > 0) { // логическое условие чтоб считать пациентов
-            double sum = 0; // еще одна переменная для среднего арифметического
-            for (int j = 0; j < pacients.length; j++) { // перебираем массив
-                sum += pacients[j]; // суммируем все полученные температуры
-            }
-            average = sum / pacients.length; // делим сумму полученных температур на кол-во пациентов
+        if (instruction.matches("delete")&& todoList.size() > 0)
+        {
+            for(int lastIndex = 0; lastIndex < todoList.size(); lastIndex++)
+                todoList.remove(lastIndex);
+            scanner.next();
         }
-        System.out.println("Средняя температура по больнице: " + average); // выводим
-        System.out.println("Здоровых пациентов: " + countHealty);
+        if(instruction.matches("edit"))
+        {
+            todoList.set(todoList.indexOf(information),element);
+            scanner.next();
         }
-    public static double rnd(double minTemperature, double maxTemperature) { // метод который задает рандомную температуру
-        double diff = (maxTemperature - minTemperature) + 1;
-        return (Math.random() * diff) + minTemperature;
     }
 }
-
 //        String[][] misterIks = {
 //                {"X", " ", " ", " ", " ", " ", "X"},
 //                {" ", "X", " ", " ", " ", "X", " "},
