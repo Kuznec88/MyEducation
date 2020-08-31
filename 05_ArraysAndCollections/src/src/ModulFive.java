53b89ec1042a1f68ec65cf42f63d6f307954c3bf
Your Name <89165418685@mail.ru>
2020-09-01 02:28:11 +0300

    Lesson 5.2

--- 05_ArraysAndCollections/src/src/ModulFive.java
+++ 05_ArraysAndCollections/src/src/ModulFive.java
@@ -4,55 +4,44 @@ import java.util.Scanner;
 public class ModulFive {
     public static void main(String[] args) {
 
-        ArrayList<String> todoList = new ArrayList<>();
+        ArrayList<String[]> todoList = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите команду: ");
-        String instruction = scanner.nextLine();
-        String information = scanner.nextLine();
-        String element = scanner.nextLine();
 
-// todoList.add("Привет");
-// todoList.add("Как дела");
-//todoList.remove(0);
+        while (true) {
+            String instruction = scanner.nextLine();
 
-        if (instruction.matches("list"))
-        {
-            todoList.size();
-            System.out.println("Список дел: " + todoList.toString()); // сделать чтоб вводилось кореектно
-            scanner.next();
-        }
+//             todoList.add("Привет");
+//             todoList.add("Как дела");
 
-        if (instruction.matches("add"))
-        {
-            todoList.add(information);
-        }
-        if (instruction.matches("delete")&& todoList.size() > 0)
-        {
-            for(int lastIndex = 0; lastIndex < todoList.size(); lastIndex++)
-                todoList.remove(lastIndex);
-            scanner.next();
-        }
-        if(instruction.matches("edit"))
-        {
-            todoList.set(todoList.indexOf(information),element);
-            scanner.next();
+            if (instruction.matches("list")) {
+                todoList.size();
+                for(String[] sizeList : todoList)
+                    System.out.println("Список дел: " + sizeList);
+            }
+
+            if (instruction.matches("add")) {
+                String[] cifra = instruction.split("\\d");
+
+                for(int i = 0; i < cifra.length; i++)
+                {
+                    todoList.add(i, instruction.split("\\S"));
+                    System.out.println("Добавлено дело " + todoList.size());
+                }
+
+            }
+            if (instruction.matches("delete") && todoList.size() > 0) {
+
+            }
+            if(todoList.isEmpty())
+            {
+                System.out.println("Список пуст");
+            }
+//            if (instruction.matches("edit")) {
+//                todoList.set(todoList.indexOf(information), element);
+//                scanner.next();
+//            }
         }
     }
 }
-//        String[][] misterIks = {
-//                {"X", " ", " ", " ", " ", " ", "X"},
-//                {" ", "X", " ", " ", " ", "X", " "},
-//                {" ", " ", "X", " ", "X", " ", " "},
-//                {" ", " ", " ", "X", " ", " ", " "},
-//                {" ", " ", "X", " ", "X", " ", " "},
-//                {" ", "X", " ", " ", " ", "X", " "},
-//                {"X", " ", " ", " ", " ", " ", "X"},
-//
-//        };
-//        for (int i = 0; i < misterIks.length; i++) { //идём по строкам
-//            for (int j = 0; j < misterIks[i].length; j++) {//идём по столбцам
-//                System.out.print(" " + misterIks[i][j] + " "); //вывод элемента
-//            }
-//            System.out.println();//перенос строки ради визуального сохранения табличной формы
-//        }
