4972d88f2e53279066b44158cb398b6ac328bfa0
Your Name <89165418685@mail.ru>
2020-09-02 15:37:27 +0300

    Lesson 5.2 fix

--- 05_ArraysAndCollections/src/src/ModulFive.java
+++ 05_ArraysAndCollections/src/src/ModulFive.java
@@ -4,44 +4,39 @@ import java.util.Scanner;
 public class ModulFive {
     public static void main(String[] args) {
 
-        ArrayList<String[]> todoList = new ArrayList<>();
+        ArrayList<String> todoList = new ArrayList<>(); // создаем аррайлист хранящий строки
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите команду: ");
+        Scanner scanner = new Scanner(System.in); // создаем сканнер чтоб считывать ввод строк
+        System.out.println("Введите команду: "); // выводим что будет передсканнером
 
-        while (true) {
-            String instruction = scanner.nextLine();
+        while (true) { // бесконечный цикл
 
-//             todoList.add("Привет");
-//             todoList.add("Как дела");
+            String instruction = scanner.nextLine();// строка введенная в сканнер называется инструкция
 
-            if (instruction.matches("list")) {
-                todoList.size();
-                for(String[] sizeList : todoList)
-                    System.out.println("Список дел: " + sizeList);
+            if (instruction.matches("list")) { // если введенная строка соответсвтует list
+                for(String sizeList : todoList) // задаем еще одну переменную для корректного вывода
+                    System.out.println("Список дел: " + sizeList); // выводим
             }
-
-            if (instruction.matches("add")) {
-                String[] cifra = instruction.split("\\d");
-
-                for(int i = 0; i < cifra.length; i++)
-                {
-                    todoList.add(i, instruction.split("\\S"));
+                if (instruction.matches("add\\s+\\d+\\s+.+")) { // если инструкция соответствует add, пробел..., целое число..., пробел..., любой символ...
+                    String[] added = instruction.split("\\s+", 3); // создаем масиив строк и разбиваем инстуркцию по 3 пробелам
+                    todoList.add(Integer.parseInt(added[1]), added[2]); // в аррайлист добавляем преобразованный в целое число номер дела и само дело?
                     System.out.println("Добавлено дело " + todoList.size());
                 }
 
-            }
-            if (instruction.matches("delete") && todoList.size() > 0) {
-
-            }
-            if(todoList.isEmpty())
-            {
-                System.out.println("Список пуст");
-            }
-//            if (instruction.matches("edit")) {
-//                todoList.set(todoList.indexOf(information), element);
-//                scanner.next();
-//            }
+                if (instruction.matches("delete\\s+\\d+") && todoList.size() > 0) { // если инструкция соотвествует delete с пробелом и любая цифра
+                    String[] deleted = instruction.split("\\s+",2); // создается массив строк и делится по 2 пробелам
+                    todoList.remove(Integer.parseInt(deleted[1])); // удаляем элемент преобразованный в инт в массиве под индексом 1
+                    System.out.println("Дело удалено"); // вывод
+                }
+                if (instruction.matches("edit\\s+\\d+\\s+.+") && todoList.size() > 0) {
+                    String[] edited = instruction.split("\\s+", 3);
+                    todoList.set(Integer.parseInt(edited[1]), edited[2]);
+                    System.out.println("Дело изменено");
+                }
+                if(todoList.isEmpty())// если аррайлист пуст
+                {
+                System.out.println("Список пуст");// выводим инфу
+                }
         }
     }
 }
