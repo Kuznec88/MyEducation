a7ad88e104fcc276cab7db00ac938cb907bd11b1
Your Name <89165418685@mail.ru>
2020-09-09 02:41:25 +0300

    5.2 final

--- 05_ArraysAndCollections/src/src/ModulFive.java
+++ 05_ArraysAndCollections/src/src/ModulFive.java
@@ -19,32 +19,35 @@ public class ModulFive {
                     System.out.println("Список дел: " + sizeList); // выводим
             }
 
-                if (instruction.matches("add\\s+\\d+\\s+.+")) {
-                    String[] added = instruction.split("\\s+", 3);
-                    if (Integer.parseInt(added[1]) <= todoList.size()) {
-                        todoList.add(Integer.parseInt(added[1]), added[2]);
-                        System.out.println("Добавлено дело " + todoList.size());
-                    }
-                } else if (instruction.matches("add\\s+.+")) {
-                    String[] secondAdd = instruction.split("\\s+", 2);
-                    todoList.add(secondAdd[1]);
+            if (instruction.matches("add\\s+\\d+\\s+.+")) {
+                String[] added = instruction.split("\\s+", 3);
+                if (Integer.parseInt(added[1]) <= todoList.size()) {
+                    todoList.add(Integer.parseInt(added[1]), added[2]);
                     System.out.println("Добавлено дело " + todoList.size());
                 }
+            } else if (instruction.matches("add\\s+.+")) {
+                String[] secondAdd = instruction.split("\\s+", 2);
+                todoList.add(secondAdd[1]);
+                System.out.println("Добавлено дело " + todoList.size());
+            }
 
-                if (instruction.matches("delete\\s+\\d+") && todoList.size() > 0) { // если инструкция соотвествует delete с пробелом и любая цифра
-                    String[] deleted = instruction.split("\\s+", 2); // создается массив строк и делится по 2 пробелам
+            if (instruction.matches("delete\\s+\\d+") && todoList.size() > 0) { // если инструкция соотвествует delete с пробелом и любая цифра
+                String[] deleted = instruction.split("\\s+", 2); // создается массив строк и делится по 2 пробелам
+                if (Integer.parseInt(deleted[1]) >= todoList.size()) {
+                    System.out.println("Неправильный номер дела");
+                } else {
                     todoList.remove(Integer.parseInt(deleted[1])); // удаляем элемент преобразованный в инт в массиве под индексом 1
                     System.out.println("Дело удалено"); // вывод
                 }
-            if (instruction.matches("edit\\s+\\d+\\s+.+")) {
-                String[] edited = instruction.split("\\s+", 3);
-                if (Integer.parseInt(edited[1]) > todoList.size()){
-                    System.out.println("Неправильный номер дела");
-                } else {
-                    todoList.set(Integer.parseInt(edited[1]), edited[2]);
-                    System.out.println("Дело изменено");
+                if (instruction.matches("edit\\s+\\d+\\s+.+")) {
+                    String[] edited = instruction.split("\\s+", 3);
+                    if (Integer.parseInt(edited[1]) >= todoList.size()) {
+                        System.out.println("Неправильный номер дела");
+                    } else {
+                        todoList.set(Integer.parseInt(edited[1]), edited[2]);
+                        System.out.println("Дело изменено");
+                    }
                 }
-            }
                 if (todoList.isEmpty())// если аррайлист пуст
                 {
                     System.out.println("Список пуст");// выводим инфу
@@ -52,3 +55,4 @@ public class ModulFive {
             }
         }
     }
+}
