e271d1df9690aaaa669167d43ad9058dd478e0cd
Your Name <89165418685@mail.ru>
2020-09-25 18:26:12 +0300

    5.4 final?

--- 05_ArraysAndCollections/src/PhoneBook/src/Main.java
+++ 05_ArraysAndCollections/src/PhoneBook/src/Main.java
@@ -1,39 +1,49 @@
+import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
-        TreeMap<String, Integer> phoneBook = new TreeMap<>(); // допустим создаем мапу
+        TreeMap<Long, String> phoneBook = new TreeMap<>(); // допустим создаем мапу
         System.out.println("Введите номер телефона, имя или команду LIST: "); // первая строчка просит ввести что нибудь
         Scanner scanner = new Scanner(System.in); // создаем сканнер
-
-//phoneBook.put("stas",898989); // это мои проверочные штуки)
 
         for (; ; ) { // создаем цикл
 
             String information = scanner.nextLine(); // делим первую строчку по цифрам
 
-            if (information.startsWith("LIST") && !phoneBook.isEmpty()) {
-                for (String vivod : phoneBook.keySet())
-                    System.out.println("Абонент " + vivod + " с номером " + phoneBook.get(vivod));
-            }else {
+            if (information.startsWith("LIST")) {
+                printMap(phoneBook);
+                continue;
+            }
+            if(!phoneBook.isEmpty() && phoneBook.containsValue(information)) {
+                System.out.println("Номер " + phoneBook.keySet() + "для абонента " + phoneBook.values());
+            }
+            if(!phoneBook.isEmpty() && phoneBook.containsKey(Long.parseLong(information)))
+            {
+                System.out.println("Номер " + phoneBook.keySet() + "для абонента " + phoneBook.values());
+            }
+            if (information.matches("\\d+") && !phoneBook.containsKey(Long.parseLong(information))) {
+                System.out.println("Такого номера нет в контактах, введите для него имя: ");
+                phoneBook.put((Long.parseLong(information)), scanner.nextLine());
+                System.out.println("В контакты добавлен номер: " + phoneBook.keySet() + "под именем " + phoneBook.values());
+            }
+            if (information.matches("\\D+") && !phoneBook.containsValue(information)) {
+                System.out.println("Такого имени нет в контактах, введите для него номер: ");
+                phoneBook.put(Long.parseLong(scanner.nextLine()), information);
+                System.out.println("В контакты добавлен номер: " + phoneBook.keySet() + "под именем " + phoneBook.values());
+            }
+            if(phoneBook.isEmpty())
+            {
                 System.out.println("Телефонная книга пуста");
             }
-            if(information.matches("\\d+") && !phoneBook.containsValue(Integer.parseInt(information))){
-                System.out.println("Такого номера нет в контактах, введите для него имя: ");
-                phoneBook.put(scanner.nextLine(), (Integer.parseInt(information)));
-                System.out.println("В контакты добавлен абонент: " + phoneBook.keySet() + "с номером " + phoneBook.values());
-            }
-            if(information.matches("\\D+") && !phoneBook.containsKey(information))
-            {
-                System.out.println("Такого имени нет в контактах, введите для него номер: ");
-                phoneBook.put(information,Integer.parseInt(scanner.nextLine()));
-                System.out.println("В контакты добавлен абонент: " + phoneBook.keySet() + "с номером " + phoneBook.values());
-            }
-            if(information.matches(phoneBook.keySet().toString()))
-            {
-                System.out.println("Абонент " + phoneBook.keySet() + " с номером " + phoneBook.values());
-            }
+        }
+    }
+    private static void printMap (Map<Long,String>map)
+    {
+        for(Long key : map.keySet())
+        {
+            System.out.println("Номер " + key + " принадлежит абоненту: " + map.get(key));
         }
     }
 }
