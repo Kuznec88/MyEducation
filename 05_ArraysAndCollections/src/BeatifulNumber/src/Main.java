f03194c5128aa2623489db0b3b2b665d9a91ffdc
kuznec88 <89165418685@mail.ru>
2020-10-02 02:48:37 +0300

    5.5 work

--- 05_ArraysAndCollections/src/BeatifulNumber/src/Main.java
+++ 05_ArraysAndCollections/src/BeatifulNumber/src/Main.java
@@ -4,16 +4,16 @@ public class Main {
     public static void main(String[] args) {
 
 
-        ArrayList<StringBuilder> beatifulNum = new ArrayList<>();
+        ArrayList<String> beatifulNum = new ArrayList<>();
         Collections.sort(beatifulNum);
-//        HashSet<String> beatifulNum2 = new HashSet<>();
-//        beatifulNum2.addAll(beatifulNum);
-//        TreeSet<StringBuilder> beatifulNum3 = new TreeSet<>();
-//        beatifulNum3.addAll(beatifulNum);
+
+        HashSet<String> beatifulNum2 = new HashSet<>();
+
+        TreeSet<String> beatifulNum3 = new TreeSet<>();
+
 
         String[] letters = {"С", "М", "Т", "В", "А", "Р", "О", "Н", "Е", "У", "Х", "К"};
         String[] digits = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
-
 
         for (int let1 = 0; let1 < letters.length; let1++) {
             for (int num1 = 0; num1 < digits.length; num1++) {
@@ -21,12 +21,10 @@ public class Main {
                     for (int num3 = 0; num3 < digits.length; num3++) {
                         for (int let2 = 0; let2 < letters.length; let2++) {
                             for (int let3 = 0; let3 < letters.length; let3++) {
-                                for (int region = 1; region <= 199; region++) {
-
-
-                                    StringBuilder sb = new StringBuilder(letters[let1] + num1 + num2 + num3 + letters[let2] + letters[let3] + region);
-                                    beatifulNum.add(sb);
-                                }
+                                String sb = letters[let1] + num1 + num2 + num3 + letters[let2] + letters[let3];
+                                beatifulNum.add(sb);
+                                beatifulNum2.add(sb);
+                                beatifulNum3.add(sb);
                             }
                         }
                     }
@@ -34,30 +32,42 @@ public class Main {
             }
         }
 
+        System.out.println("Введи интересующий номер: ");
+        Scanner scanner = new Scanner(System.in);
+        String number = scanner.nextLine();
+        int findNum = Collections.binarySearch(beatifulNum, number);
 
 
-
-//            System.out.println("Введи интересующий номер: ");
-//            Scanner scanner = new Scanner(System.in);
-//            String number = scanner.nextLine();
-//
-//            Long start = System.nanoTime();
-//            Long duration = System.nanoTime() - start;
-//
-//
-//            if (beatifulNum2.contains(number)) {
-//                System.out.println("Время поиска по хэшсету составило: " + duration + " мс");
-//            }
-//            if (beatifulNum.contains(number)) {
-//                System.out.println("Время поиска с помощью перебора составило: " + duration + " мс");
-//            }
-//            Collections.binarySearch(beatifulNum, number);
-//            {
-//                System.out.println("Время поиска с помощью бинарного поиска составило: " + duration + " мс");
-//            }
-//            if (beatifulNum3.contains(number)) {
-//                System.out.println("Время поиска по трисету составило: " + duration + " мс");
-//            }
-
+        if (beatifulNum.contains(number))
+        {
+            long start = System.nanoTime();
+            System.out.println("Такой номер существует, время поиска с помощью перебора составило: " + (System.nanoTime() - start + " мс"));
+        }else{
+            long start = System.nanoTime();
+            System.out.println("Такого номера не существует, время поиска составило: " + (System.nanoTime() - start + " мс"));
+        }
+        if (findNum > 0)
+        {
+            long start = System.nanoTime();
+            System.out.println("Такой номер существует, время поиска с помощью бинарного поиска составило: " + (System.nanoTime() - start + " мс"));
+        }else{
+            long start = System.nanoTime();
+            System.out.println("Такого номера не существует, время с помощью бинарного поиска составило: " + (System.nanoTime() - start + " мс"));
+        }
+        if (beatifulNum3.contains(number)) {
+            long start = System.nanoTime();
+            System.out.println("Такой номер существует, время поиска по трисету составило: " + (System.nanoTime() - start + " мс"));
+        }else {
+            long start = System.nanoTime();
+            System.out.println("Такого номера не существует, время поиска по трисету составило: " + (System.nanoTime() - start + " мс"));
+        }
+        if (beatifulNum2.contains(number)) {
+            long start = System.nanoTime();
+            System.out.println("Такой номер существует, время поиска по хэшсету составило: " + (System.nanoTime() - start + " мс"));
+        }else {
+            long start = System.nanoTime();
+            System.out.println("Такого номера не существует, время поиска по хэшсету составило: " + (System.nanoTime() - start + " мс"));
         }
     }
+}
+//"Время поиска по хэшсету составило: " + duration + " мс"
