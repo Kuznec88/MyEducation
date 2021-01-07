e802e758a0572684a887147d790fa77883e1a69a
Kuznec88 <89165418685@mail.ru>
2021-01-07 04:54:59 +0300

    HomeWork 8.1

--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
@@ -9,21 +9,38 @@ public class CustomerStorage
         storage = new HashMap<>();
     }
 
-    public void addCustomer(String data)
+    public void addCustomer(String data) throws Exception
     {
         String[] components = data.split("\\s+");
         String name = components[0] + " " + components[1];
         storage.put(name, new Customer(name, components[3], components[2]));
+
+        if (!components[2].contains("@"))
+        {
+            System.out.println("Email введен неверно"); // здесь еще проверки на корректность ввода
+        }
+        if (!components[3].startsWith("+7"))
+        {
+            System.out.println("Номер введен неправильно");
+        }
+
     }
 
-    public void listCustomers()
-    {
-        storage.values().forEach(System.out::println);
+    public void listCustomers() {
+        if (storage.size() == 0) {
+            System.out.println("Список пуст, сначала добавьте пользователя (list)"); // добавил проверку на наличия пользователя ы списке
+        } else {
+            storage.values().forEach(System.out::println);
+        }
     }
 
-    public void removeCustomer(String name)
-    {
-        storage.remove(name);
+    public void removeCustomer(String name) throws Exception {
+        if (storage.size() == 0) {
+            System.out.println("Список пуст, сначала добавьте пользователя (remove)");
+            // здесь тоже проверка на наличие в списке, remove & list показывает откуда приходит сообщение
+        } else {
+            storage.remove(name);
+        }
     }
 
     public int getCount()
--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/Main.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/Main.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
-public class Main
-{
+public class Main {
     private static String addCommand = "add Василий Петров " +
             "vasily.petrov@gmail.com +79215637722";
     private static String commandExamples = "\t" + addCommand + "\n" +
@@ -14,19 +13,29 @@ public class Main
     {
         Scanner scanner = new Scanner(System.in);
         CustomerStorage executor = new CustomerStorage();
-        for(;;)
-        {
+        for(;;) {
             String command = scanner.nextLine();
             String[] tokens = command.split("\\s+", 2);
-            if(tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
+            if (tokens[0].equals("add")) {
+                try {
+                    executor.addCustomer(tokens[1]);
+                    System.out.println("Добавлен пользователь: " + tokens[1]);
+                } catch (Exception e) {
+                    System.out.println("Некорректный ввод, надо так: " + addCommand);
+                }
             }
+
             else if(tokens[0].equals("list")) {
                 executor.listCustomers();
             }
             else if(tokens[0].equals("remove"))
             {
-                executor.removeCustomer(tokens[1]);
+                try {
+                    executor.removeCustomer(tokens[1]);
+                    System.out.println("Удален пользователь: " + tokens[1]); // доп. задание?
+                } catch (Exception e) {
+                    System.out.println("Введите имя пользователя согласно примеру: remove Иван Иванов");
+                }
             }
             else if(tokens[0].equals("count")) {
                 System.out.println("There are " + executor.getCount() + " customers");
